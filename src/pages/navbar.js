61256db50f4b4b06bb86f07b5756ba1baf1ad6d6
Deep Makadia <141954690+Deep1671@users.noreply.github.com>
2025-06-21 14:17:44 +0530

    Update navbar.js

--- src/pages/navbar.js
+++ src/pages/navbar.js
@@ -10,32 +10,34 @@ const NavbarComponent = () => {
         <Navbar.Toggle aria-controls="navbar-nav" />
         <Navbar.Collapse id="navbar-nav" className="justify-content-end">
           <Nav>
-            <Nav.Link href="#home" className="mx-1">
-              Home
-            </Nav.Link>
-            <Nav.Link href="#about" className="mx-1">
-              About
-            </Nav.Link>
-            <Nav.Link href="#projects" className="mx-1">
-              Projects
-            </Nav.Link>
-            <Nav.Link href="#contact" className="mx-1">
-              Contact
-            </Nav.Link>
-            <Nav.Link href="#resume" className="mx-1">
-              Resume
-            </Nav.Link>
-             <Nav.Link href="#Experience" className="mx-1">
-              Experience
-            </Nav.Link>
-            <Nav.Link href="#skills" className="mx-1">
-              Skills
-            </Nav.Link>
-            <Nav.Link href="#personal" className="mx-1">
-            Personal Information 
-            </Nav.Link>
-            <Nav.Link href="#Education" className="mx-1"> 
-            Education</Nav.Link>
+           <Nav.Link href="#home" className="mx-1">
+  Home
+</Nav.Link>
+<Nav.Link href="#about" className="mx-1">
+  About
+</Nav.Link>
+<Nav.Link href="#personal" className="mx-1">
+  Personal Information
+</Nav.Link>
+<Nav.Link href="#Education" className="mx-1">
+  Education
+</Nav.Link>
+<Nav.Link href="#contact" className="mx-1">
+  Contact
+</Nav.Link>
+<Nav.Link href="#skills" className="mx-1">
+  Skills
+</Nav.Link>
+<Nav.Link href="#resume" className="mx-1">
+  Resume
+</Nav.Link>
+<Nav.Link href="#Experience" className="mx-1">
+  Experience
+</Nav.Link>
+<Nav.Link href="#projects" className="mx-1">
+  Projects
+</Nav.Link>
+
           </Nav>
         </Navbar.Collapse>
       </Container>
