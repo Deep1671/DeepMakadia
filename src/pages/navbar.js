406edac546d9adccad3b76dd1e9d37b412bfdcb2
Deep Makadia <141954690+Deep1671@users.noreply.github.com>
2025-06-28 09:14:10 +0530

    Update navbar.js

--- src/pages/navbar.js
+++ src/pages/navbar.js
@@ -6,7 +6,7 @@ const NavbarComponent = () => {
       <Container>
         <Navbar.Brand href="#home" className="fw-bold text-primary">
        <img
-          src="qt=q_95.webp" // <-- Replace with actual image path or URL
+          src="/qt=q_95.webp" // <-- Replace with actual image path or URL
           alt="Logo"
           width="40"
           height="40"
