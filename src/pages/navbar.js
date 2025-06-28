f46942def16ad62a4c9955376d2373b5b090d7c1
Deep Makadia <141954690+Deep1671@users.noreply.github.com>
2025-06-28 09:21:25 +0530

    Update navbar.js

--- src/pages/navbar.js
+++ src/pages/navbar.js
@@ -3,18 +3,16 @@ import { Navbar, Nav, Container } from "react-bootstrap";
 const NavbarComponent = () => {
   return (
     <Navbar bg="light" expand="lg" fixed="top" className="shadow-sm">
-      <Container>
-        <Navbar.Brand href="#home" className="fw-bold text-primary">
-       <img
-          src="${process.env.PUBLIC_URL}/qt=q_95.webp" // <-- Replace with actual image path or URL
+     <Container>
+        <Navbar.Brand as={NavLink} to="/">
+        <img
+          src="qt=q_95.webp" // <-- Replace with actual image path or URL
           alt="Logo"
           width="40"
           height="40"
           className="d-inline-block align-center"
-        />  
-          Deep Makadia
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbar-nav" />
+        />  Deep Makadia</Navbar.Brand>
+        <Navbar.Toggle />
         <Navbar.Collapse id="navbar-nav" className="justify-content-end">
           <Nav>
            <Nav.Link href="#home" className="mx-1">
